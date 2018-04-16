7179f4896fc58ffef20ad02a0c0a2d96cc8dacbf
Timothy Debien <timothydebien@Timothys-MBP.domain>
2018-04-16 16:45:24 -0600

    working

--- liri.js
+++ liri.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
-var Twitter = require("twitter");
-var Spotify = require("node-spotify-api")
+
+var Spotify = require("node-spotify-api");
 var keys = require("./keys");
+var Twitter = require("twitter");
+var client = new Twitter(keys.twitter);
 var request = require("request");
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
+
 
 
 // * `my-tweets`
@@ -15,7 +18,7 @@ var spotify = new Spotify(keys.spotify);
 
 // * `do-what-it-says`
 var userInput = process.argv[2];
-console.log(process.argv);
+
 
 
 var title = process.argv.slice(3).join("+");
@@ -23,18 +26,18 @@ var title = process.argv.slice(3).join("+");
 
 // // switch statement
 switch (userInput) {
-case "my-tweets":
-    myTweets();
-    break;
-case 'spotify-this-song':
-    spotifyThis();
-    break;
-case 'movie-this':
-    movieThis();
-    break;
-case 'do-what-it-says':
-    doWhatItSays();
-    break;
+    case "my-tweets":
+        myTweets();
+        break;
+    case 'spotify-this-song':
+        spotifyThis();
+        break;
+    case 'movie-this':
+        movieThis();
+        break;
+    case 'do-what-it-says':
+        doWhatItSays();
+        break;
 }
 
 // twiter feed
@@ -42,15 +45,15 @@ function myTweets() {
     console.log('inside my tweets');
 
     // var client = new Twitter(keys.twitter);
-    var params = { screen_name: 'tj dibs' };
+    var params = { screen_name: 'TJDibs22' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             console.log(tweets);
-            for (i = 0; i < 21; i++) {
+            for (i = 0; i < tweets.length; i++) {
 
-                console.log(i  + '  ' + tweets[i].text);
+                console.log((i+1) + '  ' + tweets[i].text);
                 console.log(tweets[i].created_at);
-                
+
 
             }
         }
@@ -60,21 +63,27 @@ function myTweets() {
 
 //spotify function
 function spotifyThis() {
-    
+    // if (title == undefined) {
+
+    //     title = 'I Want it That Way'
+    // }
+
     spotify.search({ type: 'track', query: title }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
-       var track = data.tracks.items[0];
-       console.log('artists: ', track.items[0].artists[0].name);
-       console.log('song name: ', track.items[0].name);
-       console.log('song preview: ', track.items[0].artists[0].external_urls_spotify);
-       console.log('album name: ', track.items[0].album.name);
-       
+        var track = data.tracks.items[0];
+        // console.log(track);
+        console.log("~~~~~~~~~~LIRI Results~~~~~~~~~~");
+        console.log('artists: ', track.artists[0].name);
+        console.log('song name: ', track.name);
+        console.log('song preview: ', track.preview_url);
+        console.log('album name: ', track.album.name);
+        console.log("-----------------------------------");
     });
 }
-spotifyThis();
+
 
 // ```
 //        * Title of the movie.
@@ -87,48 +96,60 @@ spotifyThis();
 //        * Actors in the movie.
 //      ```
 //omdb function
-function movieThis(){
+function movieThis() {
+    if (title === undefined) {
+    
+        title = 'Billy Madison';
+    }
     var omdbURL = 'http://www.omdbapi.com/?t=' + title + '&plot=short&tomatoes=true&apikey=trilogy';
-  
-    request(omdbURL, function (error, response, body){
-     // If the request is successful
-     if(title === undefined){
-         title = 'Billy Madison';
-     }
-        //create varrible parse body of site and recover movie info
-        var body = JSON.parse(body);
 
-        console.log("Title: " + body.Title);
-        console.log("Release Year: " + body.Year);
-        console.log("IMdB Rating: " + body.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-        console.log("Country: " + body.Country);
-        console.log("Language: " + body.Language);
-        console.log("Plot: " + body.Plot);
-        console.log("Actors: " + body.Actors);
-       
- 
+    request(omdbURL, function (error, response, body) {
+        // If the request is successful
+
+        //create varrible parse body of site and recover movie info
+        var data = JSON.parse(body);
+
+        console.log("Title: " + data.Title);
+        console.log("Release Year: " + data.Year);
+        console.log("IMdB Rating: " + data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
+        console.log("Country: " + data.Country);
+        console.log("Language: " + data.Language);
+        console.log("Plot: " + data.Plot);
+        console.log("Actors: " + data.Actors);
+
+
     });
 }
 
 
 
 function doWhatItSays() {
- 
-    fs.readFile('random.txt', 'utf8', function(error, data) {
+
+    fs.readFile('random.txt', 'utf8', function (error, data) {
 
         if (error) {
             return console.log(error);
         }
         console.log(data);
-        
+        var dataArr = data.split(',')
+        console.log(dataArr);
+        var action = dataArr[0];
+        if(action === 'my-tweets'){
+            myTweets();
+        }else if(action === 'spotify-this-song'){
+            title = dataArr[1];
+            spotifyThis();
+        }else if(action === 'movie-this'){
+            movieThis();
+        }
     });
 }
 
 
 
-       
-  
+
+
 
 
 
